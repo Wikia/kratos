112e7b9356dd1482af45bc124fa1ccba7f69dd71
abador <przemyslaw@czaus.pl>
2021-10-15 14:13:19 +0200

    Additional context added to hooks, PLATFORM-6124

--- selfservice/hook/verification.go
+++ selfservice/hook/verification.go
@@ -39,7 +39,7 @@ func (e *Verifier) ExecutePostRegistrationPostPersistHook(_ http.ResponseWriter,
 	return e.do(r, s.Identity, f)
 }
 
-func (e *Verifier) ExecuteSettingsPostPersistHook(w http.ResponseWriter, r *http.Request, a *settings.Flow, i *identity.Identity) error {
+func (e *Verifier) ExecuteSettingsPostPersistHook(w http.ResponseWriter, r *http.Request, a *settings.Flow, i *identity.Identity, settingsType string) error {
 	return e.do(r, i, a)
 }
 

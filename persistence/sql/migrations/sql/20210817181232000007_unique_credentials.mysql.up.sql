9b83ef63e3e13e0da447390c9930b7aaf67606ee
abador <przemyslaw@czaus.pl>
2021-12-14 17:59:06 +0100

    Try to fix login flow for password, PLATFORM-6436

--- persistence/sql/migrations/sql/20210817181232000007_unique_credentials.mysql.up.sql
+++ persistence/sql/migrations/sql/20210817181232000007_unique_credentials.mysql.up.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX `identity_credential_identifiers_identifier_nid_type_uq_idx` ON `identity_credential_identifiers` (`identifier`, `identity_credential_type_id`, `nid`);
+CREATE UNIQUE INDEX `identity_credential_identifiers_identifier_nid_type_uq_idx` ON `identity_credential_identifiers` (`nid`, `identity_credential_type_id`, `identifier`);

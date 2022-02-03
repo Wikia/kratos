45a0d35bf57d4037979bf0da08c6cf69094f01de
Michał Kalinowski <mkalinowski@fandom.com>
2022-02-03 10:11:57 +0100

    PLATFORM-6307 Change HTTP methods string literals to constants

--- x/router.go
+++ x/router.go
@@ -1,6 +1,9 @@
 package x
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
 type RouterAdmin struct {
 	*httprouter.Router
@@ -22,32 +25,32 @@ func NewRouterAdmin() *RouterAdmin {
 	}
 }
 
-// GET is a shortcut for router.Handle("GET", path, handle)
+// GET is a shortcut for router.Handle(http.MethodGet, path, handle)
 func (r *RouterPublic) GET(path string, handle httprouter.Handle) {
-	r.Handle("GET", path, NoCacheHandler(handle))
+	r.Handle(http.MethodGet, path, NoCacheHandler(handle))
 }
 
-// HEAD is a shortcut for router.Handle("HEAD", path, handle)
+// HEAD is a shortcut for router.Handle(http.MethodHead, path, handle)
 func (r *RouterPublic) HEAD(path string, handle httprouter.Handle) {
-	r.Handle("HEAD", path, NoCacheHandler(handle))
+	r.Handle(http.MethodHead, path, NoCacheHandler(handle))
 }
 
-// POST is a shortcut for router.Handle("POST", path, handle)
+// POST is a shortcut for router.Handle(http.MethodPost, path, handle)
 func (r *RouterPublic) POST(path string, handle httprouter.Handle) {
-	r.Handle("POST", path, NoCacheHandler(handle))
+	r.Handle(http.MethodPost, path, NoCacheHandler(handle))
 }
 
-// PUT is a shortcut for router.Handle("PUT", path, handle)
+// PUT is a shortcut for router.Handle(http.MethodPut, path, handle)
 func (r *RouterPublic) PUT(path string, handle httprouter.Handle) {
-	r.Handle("PUT", path, NoCacheHandler(handle))
+	r.Handle(http.MethodPut, path, NoCacheHandler(handle))
 }
 
-// PATCH is a shortcut for router.Handle("PATCH", path, handle)
+// PATCH is a shortcut for router.Handle(http.MethodPatch, path, handle)
 func (r *RouterPublic) PATCH(path string, handle httprouter.Handle) {
-	r.Handle("PATCH", path, NoCacheHandler(handle))
+	r.Handle(http.MethodPatch, path, NoCacheHandler(handle))
 }
 
-// DELETE is a shortcut for router.Handle("DELETE", path, handle)
+// DELETE is a shortcut for router.Handle(http.MethodDelete, path, handle)
 func (r *RouterPublic) DELETE(path string, handle httprouter.Handle) {
-	r.Handle("DELETE", path, NoCacheHandler(handle))
+	r.Handle(http.MethodDelete, path, NoCacheHandler(handle))
 }

9072ee58a7f357001febe98e45177d5567b1d77d
Michał Kalinowski <mkalinowski@fandom.com>
2022-02-03 10:30:25 +0100

    PLATFORM-6307 Add /kratos-public path prefix

--- x/router.go
+++ x/router.go
@@ -25,32 +25,43 @@ func NewRouterAdmin() *RouterAdmin {
 	}
 }
 
+const RoutePathPrefix = "/kratos-public"
+
+func effectivePath(path string) string {
+	return RoutePathPrefix + path
+}
+
 // GET is a shortcut for router.Handle(http.MethodGet, path, handle)
 func (r *RouterPublic) GET(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodGet, path, NoCacheHandler(handle))
+	r.Handle(http.MethodGet, effectivePath(path), NoCacheHandler(handle))
 }
 
 // HEAD is a shortcut for router.Handle(http.MethodHead, path, handle)
 func (r *RouterPublic) HEAD(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodHead, path, NoCacheHandler(handle))
+	r.Handle(http.MethodHead, effectivePath(path), NoCacheHandler(handle))
+}
+
+// OPTIONS is a shortcut for router.Handle(http.MethodOptions, path, handle)
+func (r *RouterPublic) OPTIONS(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodOptions, effectivePath(path), handle)
 }
 
 // POST is a shortcut for router.Handle(http.MethodPost, path, handle)
 func (r *RouterPublic) POST(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodPost, path, NoCacheHandler(handle))
+	r.Handle(http.MethodPost, effectivePath(path), NoCacheHandler(handle))
 }
 
 // PUT is a shortcut for router.Handle(http.MethodPut, path, handle)
 func (r *RouterPublic) PUT(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodPut, path, NoCacheHandler(handle))
+	r.Handle(http.MethodPut, effectivePath(path), NoCacheHandler(handle))
 }
 
 // PATCH is a shortcut for router.Handle(http.MethodPatch, path, handle)
 func (r *RouterPublic) PATCH(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodPatch, path, NoCacheHandler(handle))
+	r.Handle(http.MethodPatch, effectivePath(path), NoCacheHandler(handle))
 }
 
 // DELETE is a shortcut for router.Handle(http.MethodDelete, path, handle)
 func (r *RouterPublic) DELETE(path string, handle httprouter.Handle) {
-	r.Handle(http.MethodDelete, path, NoCacheHandler(handle))
+	r.Handle(http.MethodDelete, effectivePath(path), NoCacheHandler(handle))
 }

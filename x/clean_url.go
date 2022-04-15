eea7646b4e40a09e89285a30f426b74c8647e7d1
Przemysław Czaus <przemyslaw@czaus.pl>
2022-04-15 08:59:26 +0200

    Update x/clean_url.go
    Co-authored-by: Łukasz Harasimowicz <dev@harnash.eu>

--- x/clean_url.go
+++ x/clean_url.go
@@ -12,7 +12,6 @@ func CleanPath(stripPaths []string) func(rw http.ResponseWriter, r *http.Request
 	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		r.URL.Path = httprouter.CleanPath(r.URL.Path)
 		for _, prefix := range stripPaths {
-			fmt.Println(prefix)
 			if strings.HasPrefix(r.URL.Path, prefix) {
 				r.URL.Path = strings.Replace(r.URL.Path, prefix, "", 1)
 				fmt.Println(r.URL.Path)

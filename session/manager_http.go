267346b7a13cc532fd85f8b7101621ffa94f8b6d
apasternak <apasternak@fandom.com>
2024-02-16 08:52:55 +0100

    PLATFORM-9043 Allow new Kratos v0.10.1 to recognize old session tokens - remove our changes to verify if we need them

--- session/manager_http.go
+++ session/manager_http.go
@@ -207,58 +207,16 @@ func (s *ManagerHTTP) extractToken(r *http.Request) string {
 		return token
 	}
 
-	_, err := s.getCookie(r)
+	cookie, err := s.getCookie(r)
 	if err != nil {
 		token, _ := bearerTokenFromRequest(r)
 		return token
-	}
-
-	// fandom-start support old cookie format
-	copyR := r.WithContext(r.Context())
-	copyR2 := r.WithContext(r.Context())
-	cookie, err := s.r.CookieManager(copyR2.Context()).Get(copyR2, s.cookieName(copyR2.Context()))
-	if err != nil {
-		legacyCookie, err := s.r.LegacyCookieManager(copyR.Context()).Get(copyR, s.cookieName(copyR.Context()))
-		/**
-		 * This is a workaround around shared CookieStore (Sic!)
-		 * Even creating new object sets some shared state via gorilla.sessions library
-		 * Creating old CookieManager sets shared state to previous values
-		 * THIS IS SOO BROKEN :D
-		 */
-		_, _ = s.r.CookieManager(copyR2.Context()).Get(copyR2, s.cookieName(copyR2.Context()))
-		if err != nil {
-			token, _ := bearerTokenFromRequest(r)
-			return token
-		}
-		token, ok := legacyCookie.Values["session_token"].(string)
-		if ok {
-			return token
-		}
 	}
 
 	token, ok := cookie.Values["session_token"].(string)
 	if ok {
 		return token
 	}
-
-	legacyCookie, err := s.r.LegacyCookieManager(copyR.Context()).Get(copyR, s.cookieName(copyR.Context()))
-	/**
-	 * This is a workaround around shared CookieStore (Sic!)
-	 * Even creating new object sets some shared state via gorilla.sessions library
-	 * Creating old CookieManager sets shared state to previous values
-	 * THIS IS SOO BROKEN :D
-	 */
-	_, _ = s.r.CookieManager(copyR2.Context()).Get(copyR2, s.cookieName(copyR2.Context()))
-	if err != nil {
-		token, _ := bearerTokenFromRequest(r)
-		return token
-	}
-
-	token, ok = legacyCookie.Values["session_token"].(string)
-	if ok {
-		return token
-	}
-	// fandom-end
 
 	token, _ = bearerTokenFromRequest(r)
 	return token
